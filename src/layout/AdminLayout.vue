ab365bf5f52b1ea0857a677aae3c32e17dcaf61c
kumduy <anhduy.tran160106@gmail.com>
2025-03-09 16:51:27 +0700

    no com

---
+++ src/layout/AdminLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="h-screen bg-gray-400 flex gap-3">
+      <!-- Sidebar cố định -->
+      <Sidebar class="rounded-lg p-4" />
+  
+      <!-- Nội dung mở rộng -->
+      <div class="bg-gray-100 flex-grow p-4">
+        <router-view />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import Sidebar from "../components/Sidebar.vue";
+  </script>
+  
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,23 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
-
+import Profile from "../views/Profile.vue";
+import SanPham from "../views/SanPham.vue";
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile,
+    },
+    {
+        path: '/san-pham',
+        name: 'SanPham',
+        component: SanPham,
     },
     {
         path: '/login',
