22a8397e872b56855174caadeb01492ccd9d0123
LDragon3926 <ldragon3919@gmail.com>
2025-03-21 22:44:50 +0700

    10h44 21/3

--- src/views/ThongKe/TTKH.vue
+++ src/views/ThongKe/TTKH.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from "axios";
 import { Line } from 'vue-chartjs'
 import {
   Chart as ChartJS,
@@ -15,6 +16,9 @@ ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, CategoryScal
 
 export default {
   components: { Line },
+  props: {
+    isSidebarOpen: Boolean, // Nhận trạng thái sidebar từ component cha
+  },
   data() {
     return {
       chartData: {
@@ -32,19 +36,66 @@ export default {
       },
       chartOptions: {
         responsive: true,
+        maintainAspectRatio: false, // Cho phép biểu đồ thay đổi theo container
         plugins: {
           legend: {
-            position: 'bottom'
-          }
-        }
-      }
+            position: "bottom",
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+      observer: null, // ResizeObserver để theo dõi kích thước
+    };
+  },
+  mounted() {
+    this.fetchData();
+    this.initResizeObserver();
+  },
+  watch: {
+    isSidebarOpen() {
+      this.$nextTick(() => {
+        this.$forceUpdate(); // Cập nhật lại biểu đồ khi sidebar thay đổi
+      });
+    },
+  },
+  beforeUnmount() {
+    if (this.observer) {
+      this.observer.disconnect();
     }
-  }
+  },
+
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get("http://localhost:8080/api/khach-hang/");
+        this.chartData = response.data;
+      } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu từ API:", error);
+      }
+    },
+
+    initResizeObserver() {
+      const container = this.$refs.chartContainer;
+      if (container) {
+        this.observer = new ResizeObserver(() => {
+          this.$forceUpdate(); // Cập nhật lại biểu đồ khi container thay đổi kích thước
+        });
+        this.observer.observe(container);
+      }
+    },
+  },
+  
 }
 </script>
 
 <template>
-  <div class="flex flex">
+ <div ref="chartContainer" 
+       class="transition-all duration-300 w-full h-[50vh] p-4"
+       :class="isSidebarOpen ? 'md:w-3/4' : 'md:w-full'">
     <Line :data="chartData"  :options="chartOptions" />
   </div>
 </template>
